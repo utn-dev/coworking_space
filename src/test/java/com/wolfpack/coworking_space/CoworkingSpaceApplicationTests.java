06a177c1db1d1a416e2108c90911f862bbcfaeb7
utn-dev <marcos-ezequiel-ramos@docentes.mdp.utn.edu.ar>
2025-02-17 12:51:16 -0300

    Inicializacion Proyecto SpringBoot

---
+++ src/test/java/com/wolfpack/coworking_space/CoworkingSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfpack.coworking_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

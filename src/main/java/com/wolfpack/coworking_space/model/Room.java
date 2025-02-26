6ae38e750c6a4767ec743beac55d48aa7dd08431
utn-dev <marcos-ezequiel-ramos@docentes.mdp.utn.edu.ar>
2025-02-26 12:23:28 -0300

    RoomController y BookingController mejorados: mejor manejo optional, validación en modelo y lógica de metodo update

--- src/main/java/com/wolfpack/coworking_space/model/Room.java
+++ src/main/java/com/wolfpack/coworking_space/model/Room.java
@@ -1,6 +1,7 @@
 package com.wolfpack.coworking_space.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
@@ -15,12 +16,17 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false, unique = true)
+    @NotBlank(message = "The name cannot be empty")
+    @Size(max = 100, message = "The name cannot exceed 100 characters")
+    @Column(nullable = false, unique = true)
     private String name;
 
-    @Column(name = "capacity", nullable = false)
+    @Min(value = 1, message = "Capacity must be at least 1")
+    @Max(value = 500, message = "Capacity cannot exceed 500")
+    @Column(nullable = false)
     private int capacity;
 
+    @NotNull(message = "Status is required")
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
     private RoomStatus status;
